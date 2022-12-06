8c00e5e6db80b736f2787e1d8bc6e961177a8da6
Emerson <emerson.day@gmail.com>
2022-12-06 11:10:38 -0800

    restaurant search

--- components/Restaurants.jsx
+++ components/Restaurants.jsx
@@ -1,27 +1,38 @@
 import Link from 'next/link';
 
-function Restaurants({ restaurants }) {
+function Restaurants({ restaurants, value }) {
   return (
     <>
       <ul className="list-none space-y-4 text-4xl font-bold mb-3">
         {restaurants &&
-          restaurants.data.map((restaurant) => {
-            return (
-              <li
-                className="border-2 border-teal-200 rounded-xl p-4 hover:border-teal-400"
-                key={restaurant.id}
-              >
-                <Link href={`restaurant/` + restaurant.id}>
-                  <h1 className="text-purple-600">
-                    {restaurant.attributes.name}
-                  </h1>
-                  <p className="text-lg font-light text-indigo-700">
-                    {restaurant.attributes.description}
-                  </p>
-                </Link>
-              </li>
-            );
-          })}
+          restaurants.data
+            .filter((restaurant) => {
+              if (!value) return true;
+              if (
+                restaurant.attributes.name
+                  .toLowerCase()
+                  .includes(value.toLowerCase())
+              ) {
+                return true;
+              }
+            })
+            .map((restaurant) => {
+              return (
+                <li
+                  className="border-2 border-teal-200 rounded-xl p-4 hover:border-teal-400"
+                  key={restaurant.id}
+                >
+                  <Link href={`restaurant/` + restaurant.id}>
+                    <h1 className="text-purple-600">
+                      {restaurant.attributes.name}
+                    </h1>
+                    <p className="text-lg font-light text-indigo-700">
+                      {restaurant.attributes.description}
+                    </p>
+                  </Link>
+                </li>
+              );
+            })}
       </ul>
     </>
   );
--- components/RestaurantsList.jsx
+++ components/RestaurantsList.jsx
@@ -1,15 +1,13 @@
 import Restaurants from './Restaurants';
 import { useState } from 'react';
 function RestaurantsList({ restaurants }) {
-  const [query, setQuery] = useState('');
+  const [value, setValue] = useState('');
 
-  const handleChange = (e) => {
-    setQuery(e.target.value);
-  };
+  console.log('restaurants list:', restaurants);
   return (
     <div>
       <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter mb-4">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-purple-500 py2">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-purple-500 py-2">
           Restaurants
         </span>
       </h1>
@@ -17,13 +15,13 @@ function RestaurantsList({ restaurants }) {
       <input
         type="search"
         name="search"
-        value={query}
-        onChange={handleChange}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         placeholder="Search"
-        className="md:p-2 form-input py-2 mb-4 rounded  text-slate-900"
+        className="md:p-2 form-input py-2 mb-6 rounded  text-slate-900"
       />
 
-      <Restaurants restaurants={restaurants} />
+      <Restaurants restaurants={restaurants} value={value} />
     </div>
   );
 }
